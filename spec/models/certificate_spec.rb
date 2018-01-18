adebbb89ada1555cb916e2a5396a24c3589391c1
Alexander <english3000.org@gmail.com>
2018-01-18 13:30:36 -0800

    controller tests

--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Certificate, type: :model do
-  it { should validate_presence_of(:private_key) }
-  it { should validate_presence_of(:body) }
-  it { should validate_inclusion_of(:active?).in_array([true, false]) }
+  describe 'model tests' do
+    it { should validate_presence_of(:private_key) }
+    it { should validate_presence_of(:body) }
+    it { should validate_inclusion_of(:active?).in_array([true, false]) }
 
-  it { should belong_to(:user) }
+    it { should belong_to(:user) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
+  describe 'model tests' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
 
-  it { should have_many(:certificates) }
+    it { should have_many(:certificates) }
+  end
 end
